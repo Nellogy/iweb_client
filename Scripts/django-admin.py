3e08d713a96c6d19e1c409789fe31d597c81aaa8
Nellogy <Danix-dx@hotmail.com>
2019-11-28 19:33:32 +0100

    First commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Nellogy\Desktop\iWeb\iweb_client\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ parkings/urls.py
@@ -0,0 +1,8 @@
+# to start app: python manage.py startapp polls
+from django.urls import path
+from . import views
+
+app_name = 'parkings'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
